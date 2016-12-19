206f192d103f3e8112c29290ad5db6a1405e0969
Fanha Giang <fanha99@hotmail.com>
2016-12-19 21:49:02 +0700

    caller window for apiLevel >= 19, some phone block TYPE_SYSTEM_ALERT, use TYPE_TOAST instead

--- app/src/main/java/com/odoo/addons/phonecall/features/CallerWindow.java
+++ app/src/main/java/com/odoo/addons/phonecall/features/CallerWindow.java
@@ -58,7 +58,7 @@ public class CallerWindow {
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_TOAST,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                         | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                         | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
